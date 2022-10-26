1aef9ebaef698e406d7e22b64e533fde418a3b7d
Nawaz Danish <nawazdanish1996@gmail.com>
2022-10-26 16:55:06 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 var http = require("http");
 
-const httpServer = http.createServer(handleServer);
+const httpServer = http.createServer(handleServer).listen(8081);
 
 
 function handleServer(req, res) {
-  
+  res.writeHead(200, 'Welcome to Dominos!');
+  res.end();
 }
 
 module.exports = httpServer;
